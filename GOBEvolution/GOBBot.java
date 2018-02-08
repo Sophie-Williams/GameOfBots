e604f0451bffd3b75de04d95b203757cdc0d0408
luksab <luksab@users.noreply.github.com>
2018-02-08 09:44:26 +0100

    Add GOBEvolution

---
+++ GOBEvolution/GOBBot.java
@@ -0,0 +1,28 @@
+public class GOBBot
+{
+    public Net net;
+    int sizeX = 15;
+    int sizeY = 15;
+    int[][] IDs = new int[sizeX][sizeY];
+
+    public GOBBot(float weight)
+    {
+        net = new Net();
+    }
+
+    /*public GOBBot(float weight, float[][] l0, float[][] l1, float r,float g, float b)
+    {
+    d=random(TAU);
+    s=random(1);
+    net = new Net();
+    }*/
+    void updaten() {
+        float[] arr = new float[sizeX*sizeY];
+        float[] array = new float[sizeX*sizeY];
+        for (int i=0; i<sizeX; i++)
+            for (int j=0; j<sizeY; j++)
+                array[i*sizeX+j] = IDs[i][j];
+        //println(net.update(array));
+        arr = net.update(array);
+    }
+}
---
+++ GOBEvolution/Net.java
@@ -0,0 +1,37 @@
+public class Net {
+    float[][] l0 = new float[10][10];
+    float[][] l1 = new float[l0[0].length][3];
+    public Net() {
+        for (int i=0; i<l0.length; i++) {
+            for (int j=0; j<l0[0].length; j++)
+                l0[i][j] = (float)(Math.random()*2)-1;
+        }
+        for (int i=0; i<l1.length; i++) {
+            for (int j=0; j<l1[0].length; j++)
+                l1[i][j] = (float)(Math.random()*2)-1;
+        }
+    }
+
+    public Net(float[][] ol0, float[][] ol1) {
+        l0 = ol0;
+        l1 = ol1;
+    }
+
+    public float[] update(float[] input) {
+        if (input.length != l0[0].length)
+            return new float[0];
+        float[] layer = new float[input.length]; 
+        for (int i=0; i<input.length; i++) {
+            for (int j=0; j<l0[0].length; j++)
+                layer[i] += input[i]*l0[i][j];
+        }
+        float[] out = new float[l1[0].length];
+        for (int i=0; i<l1[0].length; i++) {
+            for (int j=0; j<l1[0].length; j++)
+                out[i] +=
+                layer[i]*
+                l1[i][j];
+        }
+        return out;
+    }
+}
