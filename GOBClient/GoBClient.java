08a5c1cc1247d99ec389cce5f04559fec0725c2a
Alexander Leist <alexander.leist@p-hbg-0d201-153.schule.local>
2018-02-19 09:51:36 +0100

    arbeit am game start und etwas anderer kram

--- GOBClient/GoBClient.java
+++ GOBClient/GoBClient.java
@@ -9,6 +9,8 @@ public class GoBClient extends Client
 {
     // instance variables - replace the example below with your own
     boolean turnExecution = false;
+    boolean game = false;
+    int colour = -1;
     Queue<cellUpdate> updateTodo;
 
     /**
@@ -21,6 +23,11 @@ public class GoBClient extends Client
 
     public void processMessage(String pMessage){
         //still have to think about how to do the game start
+        if(pMessage.substring(0,1).equals("GS"))
+        {
+            colour = Character.getNumericValue(pMessage.charAt(2));
+            game = true;
+        }
         
         //turn execution
         if(pMessage.equals("ET"))
@@ -28,7 +35,7 @@ public class GoBClient extends Client
         
         /*
          * Cell Update.
-         * Format is "CU" + x-coordinate + y-coordinate (2 digits per coordinate)
+         * Format is "CU" + x-coordinate + y-coordinate + colour(3 digits per coordinate, colour is a single digit)
          * the data is then stored in a stack/queue (doesn't really matter I think) containing cellUpdate classes
          * (Ich hab zuhause keinen zugriff auf die abiturklassen)
          * the game reads that stack and updates the board accordingly
@@ -36,11 +43,19 @@ public class GoBClient extends Client
         int tempx,tempy,tempcolour;
         if(pMessage.substring(0,1).equals("CU"))
         {
-            tempx = (Character.getNumericValue(pMessage.charAt(2))*10)+(Character.getNumericValue(pMessage.charAt(3)));
-            tempy = (Character.getNumericValue(pMessage.charAt(4))*10)+(Character.getNumericValue(pMessage.charAt(5)));
-            tempcolour = Character.getNumericValue(pMessage.charAt(6));
+            tempx = (Character.getNumericValue(pMessage.charAt(2))*100)+(Character.getNumericValue(pMessage.charAt(3))*10)+(Character.getNumericValue(pMessage.charAt(4)));
+            tempy = (Character.getNumericValue(pMessage.charAt(5))*100)+(Character.getNumericValue(pMessage.charAt(6))*10)+(Character.getNumericValue(pMessage.charAt(7)));
+            tempcolour = Character.getNumericValue(pMessage.charAt(8));
             updateTodo.enqueue(new cellUpdate(tempx, tempy, tempcolour));
         }
+    }
+    
+    /*
+     * send the server a cellupdate for distribution
+     */
+    public void cellUpdate(int x, int y, int c){
+        if(100>x && x>-1 && 100>y && y>-1 && c<6 && c>-1)
+            send("CU" + x + y + c);
     }
     
     /*
--- GOBServer/GoBServer.java
+++ GOBServer/GoBServer.java
@@ -11,6 +11,7 @@ public class GoBServer extends Server
     String[] IdToIP = new String[6];
     int[] IdToPort = new int[6];
     int[] IdToColour = new int[6];
+    String[] readyPlayerIP = new String[6];
     int readyGamePlayers = 0;
     boolean game = false;
     int readyTurnPlayers = 0;
@@ -23,6 +24,7 @@ public class GoBServer extends Server
         super(6666);
         for(int j=0;j<6;j++){
             IdToIP[j] = "-1";
+            readyPlayerIP[j] = "-1";
             IdToPort[j] = -1;
         }
     }
@@ -40,15 +42,39 @@ public class GoBServer extends Server
         //Game Start
         if(stringb.substring(0,1).equals("GS"))
         {
+            /*
+             * adds the client signalling ready to the list of ready clients
+             */
             if(stringb.charAt(2) == '0')
             {
+                //checks if the client signalling ready has already signalled in the past
+                boolean tempbool = false;
+                for(int n=0;n<6;n++)
+                {
+                    if(readyPlayerIP[n].equals(stringa))
+                        tempbool = true;
+                }
                 //as a client has signaled it is ready, the count of ready players is increased by one
-                readyGamePlayers++;
+                if(tempbool = false){
+                    readyGamePlayers++;
+                    for(int l=0;l<6;l++){
+                        if(readyPlayerIP[l].equals("-1")){
+                            readyPlayerIP[l] = stringa;
+                            l = 6;
+                        }
+                    }
+                }
                 
                 //If all players are ready, the server considers the game to be started. TBI.
                 if(readyGamePlayers == 6)
                 {
                     game = true;
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 1;
+                    IdToColour[2] = 2;
+                    IdToColour[3] = 3;
+                    IdToColour[4] = 4;
+                    IdToColour[5] = 5;
                 }
             }
             
@@ -58,7 +84,70 @@ public class GoBServer extends Server
              */
             else if(stringb.charAt(2) == 'F')
             {
-                //This is gonna assign bots to the unclaimed colours in the future
+                //checks if the client signalling ready has already signalled in the past
+                boolean tempbool = false;
+                for(int n=0;n<6;n++)
+                {
+                    if(readyPlayerIP[n].equals(stringa))
+                        tempbool = true;
+                }
+                //as a client has signaled it is ready, the count of ready players is increased by one
+                if(tempbool = false){
+                    readyGamePlayers++;
+                    for(int l=0;l<6;l++){
+                        if(readyPlayerIP[l].equals("-1")){
+                            readyPlayerIP[l] = stringa;
+                            l = 6;
+                        }
+                    }
+                }
+                
+                if(readyGamePlayers == 1){
+                    IdToColour[0] = 0;
+                    //bots
+                }
+                else if(readyGamePlayers == 2){
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 3;
+                    //bots
+                }
+                else if(readyGamePlayers == 3){
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 2;
+                    IdToColour[2] = 4;
+                    //bots
+                }
+                else if(readyGamePlayers == 4){
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 1;
+                    IdToColour[2] = 3;
+                    IdToColour[3] = 4;
+                    //bots
+                }
+                else if(readyGamePlayers == 5){
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 1;
+                    IdToColour[2] = 2;
+                    IdToColour[3] = 3;
+                    IdToColour[4] = 4;
+                    //bots
+                }
+                else if(readyGamePlayers == 6){
+                    IdToColour[0] = 0;
+                    IdToColour[1] = 1;
+                    IdToColour[2] = 2;
+                    IdToColour[3] = 3;
+                    IdToColour[4] = 4;
+                    IdToColour[5] = 5;
+                }
+                game = true;
+            }
+            
+            if(game == true){
+                for(int m=0;i<6;i++){
+                    if(IdToPort[m] != -1)
+                        send(IdToIP[m], IdToPort[m], "GS" + IdToColour[m]);
+                }
             }
         }
         
