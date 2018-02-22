f5e79c385dd828a2a8cb4e9e4f182d6bc4f280b8
luksab <luksab@users.noreply.github.com>
2018-02-22 08:44:35 +0100

    add 3rd layer to Net

--- GOBEvolution/Net.java
+++ GOBEvolution/Net.java
@@ -1,7 +1,8 @@
 public class Net { //a fully connected neural net for GOB
-    float[][] l0 = new float[10][10];
-    float[][] l1 = new float[l0[0].length][3];
-    public Net() {
+    float[][] l0 = new float[50][100];//1st layer:Playfield * 1st internal
+    float[][] l1 = new float[l0[0].length][100];//2nd layer:1st internal* 2nd internal
+    float[][] l2 = new float[l1[0].length][50];//2nd layer:2nd internal* Playfield
+    public Net() {//initialize randomly from -1 to 1
         for (int i=0; i<l0.length; i++) {
             for (int j=0; j<l0[0].length; j++)
                 l0[i][j] = (float)(Math.random()*2)-1;
@@ -10,16 +11,23 @@ public class Net { //a fully connected neural net for GOB
             for (int j=0; j<l1[0].length; j++)
                 l1[i][j] = (float)(Math.random()*2)-1;
         }
+        for (int i=0; i<l2.length; i++) {
+            for (int j=0; j<l2[0].length; j++)
+                l2[i][j] = (float)(Math.random()*2)-1;
+        }
     }
 
-    public Net(float[][] ol0, float[][] ol1) {
+    public Net(float[][] ol0, float[][] ol1, float[][] ol2) {//copy other Net
         l0 = ol0;
         l1 = ol1;
+        l2 = ol2;
     }
     
-    public Net(float[][] ol0, float[][] ol1, float[][] old0, float[][] old1) {
+    public Net(float[][] ol0, float[][] ol1, float[][] ol2
+    , float[][] old0, float[][] old1, float[][] old2) {//intitialize from two Nets by calculating the mean
         l0 = ol0;
         l1 = ol1;
+        l2 = ol2;
         for (int i=0; i<l0.length; i++) {
             for (int j=0; j<l0[0].length; j++)
                 l0[i][j] = (l0[i][j]+old0[i][j])/2;
@@ -28,9 +36,13 @@ public class Net { //a fully connected neural net for GOB
             for (int j=0; j<l1[0].length; j++)
                 l1[i][j] = (l1[i][j]+old1[i][j])/2;
         }
+        for (int i=0; i<l2.length; i++) {
+            for (int j=0; j<l2[0].length; j++)
+                l2[i][j] = (l2[i][j]+old2[i][j])/2;
+        }
     }
     
-    public void randomize(double amount) {
+    public void randomize(double amount) {//randomize the layers by amount
         for (int i=0; i<l0.length; i++) {
             for (int j=0; j<l0[0].length; j++)
                 l0[i][j] = l0[i][j]+(float)((Math.random()*amount)-(amount/2));
@@ -38,6 +50,10 @@ public class Net { //a fully connected neural net for GOB
         for (int i=0; i<l1.length; i++) {
             for (int j=0; j<l1[0].length; j++)
                 l1[i][j] = l1[i][j]+(float)((Math.random()*amount)-(amount/2));
+        }
+        for (int i=0; i<l2.length; i++) {
+            for (int j=0; j<l2[0].length; j++)
+                l2[i][j] = l2[i][j]+(float)((Math.random()*amount)-(amount/2));
         }
     }
 
@@ -49,12 +65,19 @@ public class Net { //a fully connected neural net for GOB
             for (int j=0; j<l0[0].length; j++)
                 layer[i] += input[i]*l0[i][j];
         }
-        float[] out = new float[l1[0].length];
+        float[] internal = new float[l1[0].length];
         for (int i=0; i<l1[0].length; i++) {
             for (int j=0; j<l1[0].length; j++)
-                out[i] +=
+                internal[i] +=
                 layer[i]*
                 l1[i][j];
+        }
+        float[] out = new float[l2[0].length];
+        for (int i=0; i<l2[0].length; i++) {
+            for (int j=0; j<l2[0].length; j++)
+                out[i] +=
+                layer[i]*
+                l2[i][j];
         }
         return out;
     }
